acd02769a4af7b6e643ad02059002c5e1d46206e
zoupengyu <zoupengyu@qunjielong.com>
2022-01-18 15:24:30 +0800

    fix

--- BasicSpringMvc/src/main/java/com/slim/basic/spring/mvc/config/loadConfig/EnableTestBeanValueConfig.java
+++ BasicSpringMvc/src/main/java/com/slim/basic/spring/mvc/config/loadConfig/EnableTestBeanValueConfig.java
@@ -1,6 +1,5 @@
 package com.slim.basic.spring.mvc.config.loadConfig;
 
-import com.slim.basic.spring.mvc.config.loadConfig.TestBeanValueConfig;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
