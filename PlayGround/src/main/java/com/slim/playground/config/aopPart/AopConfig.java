9c29011a4a700c351d5a36dfb6e4664f17c1df0f
zoupengyu <zoupengyu@qunjielong.com>
2022-03-04 13:04:27 +0800

    fix

--- PlayGround/src/main/java/com/slim/playground/config/aopPart/AopConfig.java
+++ PlayGround/src/main/java/com/slim/playground/config/aopPart/AopConfig.java
@@ -1,11 +1,13 @@
 package com.slim.playground.config.aopPart;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 @EnableAspectJAutoProxy
 @Import(value = {CalculateAspect.class})
+@Configuration
 public class AopConfig {
     @Bean
     public CalculateComponent calculateComponent() {
